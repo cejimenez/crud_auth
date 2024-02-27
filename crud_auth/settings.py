180a06d9db28931ba99b2c75edce0d13e24ba6ff
cejimenez <cristian@cjconsultora.cl>
2024-02-27 09:31:49 -0300

    Proyecto para render.com

--- crud_auth/settings.py
+++ crud_auth/settings.py
@@ -24,9 +24,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jp_zuf3aet*w@9^_jq*0znq#iz0&2i3y--dq$3utp3omrkb^bg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
